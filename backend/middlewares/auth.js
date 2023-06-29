0cf189f553c6fb580446c94a58f4e33b1297d01f
Svetlana <kuznetsova@66.ru>
2023-06-29 12:36:11 +0500

    hotfix v2

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-err');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET = 'JWT_SECRET' } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
